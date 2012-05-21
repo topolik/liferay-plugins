c489b52f65114c51d5edc7c073a8fc3530e8e2a2
Igor Beslic <igor.beslic@liferay.com>
2012-05-21 14:23:20 +0200

    LPS-26751 added custom finders for OAuthApplication

---
+++ webs/oauth-web/docroot/WEB-INF/src/com/liferay/portal/oauth/service/persistence/OAuthApplicationFinderImpl.java
@@ -0,0 +1,7 @@
+package com.liferay.portal.oauth.service.persistence;
+
+import com.liferay.portal.oauth.model.OAuthApplication;
+import com.liferay.portal.service.persistence.impl.BasePersistenceImpl;
+
+public class OAuthApplicationFinderImpl
+	extends BasePersistenceImpl<OAuthApplication> {}
